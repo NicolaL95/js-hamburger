1b65a6eade96b1de216099b9e60272dff3a8a47a
NicolaL95 <nicola.lilla@gmail.com>
2021-10-12 17:36:49 +0200

    Correzione in corso

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ document.querySelector("a > i").addEventListener("click", openbutton);
 
 
 function openbutton() {
-    document.querySelector(".hamburger-menu").style.display = "block";
+    document.querySelector(".hamburger-menu").className = "active"
 }
 
 document.querySelector(".close").addEventListener("click", closecross);
