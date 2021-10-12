ee5c720ebe4f5a265754207a3bcdef63c43bf1dc
NicolaL95 <nicola.lilla@gmail.com>
2021-10-12 17:57:22 +0200

    Fine lavorazione

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 document.querySelector("a > i").addEventListener("click", openbutton);
 
+var element = document.getElementsByClassName("hamburger-menu")[0];
 
 function openbutton() {
-    /* document.querySelector(".hamburger-menu").className = "active" */
-    var element = document.getElementById("test");
-    element.className = "active";
+    element.classList.add("active")
 }
 
 document.querySelector(".close").addEventListener("click", closecross);
 
 
 function closecross() {
-    var element = document.getElementById("test");
     element.classList.remove("active")
 }
